e5b009c1969c0a4db7b5e6ed4c05b5126a931d2a
Mateen Shahbaz Ahmed <75861907+MateenAhmed10@users.noreply.github.com>
2022-10-31 16:02:57 +0500

    Class 2 React Assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello from React.js tutorial
-        </h1>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <header className="App__header box">
+            <h1>
+                Bootcamp 2020, Class 2 React
+            </h1>
+            <p>
+                Hello from Mateen Shahbaz Ahmed
+            </p>
+        </header>
+    );
+};
+
+
+export default Header;
